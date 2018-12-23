adc169b7d26da8507f2b2480010e17635dda135b
Alexandre Guidet <alex@room9.co.nz>
2018-12-24 09:41:03 +1300

    precommit hook for linting

---
+++ jestSetup.js
@@ -0,0 +1,28 @@
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+// ----------------------------------------------------------------
+// mock fetch to enable fake http calls in tests
+// ----------------------------------------------------------------
+global.fetch = require('jest-fetch-mock')
+
+// ----------------------------------------------------------------
+// mock the native gesture handler to run in a test environment
+// ----------------------------------------------------------------
+jest.mock('react-navigation', () => {
+  return {
+    createAppContainer: jest.fn().mockReturnValue(function NavigationContainer(props) {
+      return null
+    }),
+    createDrawerNavigator: jest.fn(),
+    createMaterialTopTabNavigator: jest.fn(),
+    createStackNavigator: jest.fn(),
+    StackActions: {
+      push: jest.fn().mockImplementation(x => ({ ...x, type: 'Navigation/PUSH' })),
+      replace: jest.fn().mockImplementation(x => ({ ...x, type: 'Navigation/REPLACE' })),
+    },
+    NavigationActions: {
+      navigate: jest.fn().mockImplementation(x => x),
+    },
+  }
+})
